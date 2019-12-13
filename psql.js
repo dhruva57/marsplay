6ae7a401c59d2f81a92e8a5ba355b599162d5557
Dhruva Tripathi <tripathi.dhruva05@gmail.com>
2019-12-13 10:22:26 +0530

    complete psql

--- psql.js
+++ psql.js
@@ -2,27 +2,241 @@ const {  Client } = require('pg');
 const  axios  = require('axios');
 const fs = require('fs');
 
-const sql = fs.readFileSync('schema.sql').toString();
-const connectionString = 'postgres://fchhltfc:vcyFNRGGWwzAIXdQPiTMXcEXz5nYAYLV@satao.db.elephantsql.com:5432/fchhltfc'
+const query = fs.readFileSync('schema.sql').toString();
 
-const client = new Client({
-  connectionString: connectionString,
-});
-client.connect();
-let query = 'SELECT * FROM companies;';
-// let query = 'SELECT * FROM pg_catalog.pg_tables;';
-client.query(sql, (err, res) => {
-  console.log(err, res)
-  client.end();
+const connectionString = 'postgres://fchhltfc:vcyFNRGGWwzAIXdQPiTMXcEXz5nYAYLV@satao.db.elephantsql.com:5432/fchhltfc';
+
+const con = new Client({
+  connectionString: connectionString
 });
 
-// getUser();
+con.connect(async function (err) {
+  if (err) return console.error(err);
+  console.log("Successfully connected");
+  await runQuery(query);
+  await getUsers();
+  await getPosts();
+  await getComments();
+  await getAlbums();
+  await getPhotos();
+  await getTodos();
+  con.end();
+});
 
-async function getUser() {
+function runQuery(sql) {
+  return new Promise((resolve, reject) => {
+    console.log("query is", sql);
+    con.query(sql, function (err, result) {
+      if (err) reject(err);
+      // console.log("result is", result);
+      return resolve(result);
+    });
+  });
+}
+
+async function getPhotos() {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    console.log(response.data);
+    const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
+    for (let photo of response.data) {
+      await addPhoto(photo);
+    }
   } catch (error) {
     console.error(error);
   }
 }
+
+async function getTodos() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    for (let todo of response.data) {
+      await addTodo(todo);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getAlbums() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/albums');
+    for (let album of response.data) {
+      await addAlbum(album);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getComments() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
+    for (let comment of response.data) {
+      await addComment(comment);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getPosts() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    for (let post of response.data) {
+      await addPost(post);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getUsers() {
+  try {
+    let userId, addressId;
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    for (let user of response.data) {
+      userId = await addUser(user);
+      addressId = await addAddress(user, userId);
+      await addGeo(user, addressId);
+      await addCompany(user, userId);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addUser(user) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO users_dhruva (u_name,u_username,u_email,u_website,u_phone) VALUES ('${user.name}','${user.username}','${user.email}','${user.website}','${user.phone}')`;
+
+      let response = await runQuery(sql);
+      if (response) {
+        sql = `SELECT u_id FROM users_dhruva WHERE u_username = '${user.username}'`;
+
+        let userId = await runQuery(sql);
+        if (userId.rows[0].u_id) return resolve(userId.rows[0].u_id);
+      }
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addPost(post) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO posts_dhruva (p_userId,p_title,p_body) VALUES ('${post.userId}','${post.title}','${post.body}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addTodo(todo) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO todos_dhruva (t_userId,t_title,t_completed) VALUES ('${todo.userId}','${todo.title}','${todo.completed}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addAlbum(album) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO albums_dhruva (a_userId,a_title) VALUES ('${album.userId}','${album.title}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addPhoto(photo) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO photos_dhruva (p_albumId,p_title,p_url,p_thumbnailUrl) VALUES ('${photo.albumId}','${photo.title}','${photo.url}','${photo.thumbnailUrl}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addComment(comment) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO comments_dhruva (c_postId,c_name,c_email,c_body) VALUES ('${comment.postId}','${comment.name}','${comment.email}','${comment.body}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addAddress(user, userId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO userAddress_dhruva (ua_userId,ua_street,ua_suite,ua_city,ua_zipcode) VALUES ('${userId}','${user.address.street}','${user.address.suite}','${user.address.city}','${user.address.zipcode}')`;
+
+      let response = await runQuery(sql);
+      if (response) {
+        sql = `SELECT ua_id FROM userAddress_dhruva WHERE ua_userId = '${userId}'`;
+
+        let addressId = await runQuery(sql);
+        if (addressId.rows[0].ua_id) return resolve(addressId.rows[0].ua_id);
+      } 
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addGeo(user, addressId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO userAddressGeo_dhruva (uag_addressId,uag_lat,uag_lng) VALUES ('${addressId}','${user.address.geo.lat}','${user.address.geo.lng}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
+async function addCompany(user, userId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let sql = `INSERT INTO userCompany_dhruva (uc_userId,uc_name,uc_catchPhrase,uc_bs) VALUES ('${userId}','${user.company.name}','${user.company.catchPhrase}','${user.company.bs}')`;
+
+      let response = await runQuery(sql);
+      if (response) return resolve(true);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+  });
+}
+
--- schema.sql
+++ schema.sql
@@ -1,77 +1,104 @@
-DROP TABLE IF EXISTS `users_dhruva`;
-CREATE TABLE `users_dhruva` (
-  `u_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `u_name` varchar(36) NOT NULL,
-  `u_username` varchar(20) NOT NULL UNIQUE,
-  `u_email` varchar(36) NOT NULL,
-  `u_website` varchar(36) NOT NULL,
-  `u_phone` varchar(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS users_dhruva;
+DROP SEQUENCE IF EXISTS users_dhruva_seq;
+CREATE SEQUENCE users_dhruva_seq;
 
-DROP TABLE IF EXISTS `userAddress_dhruva`;
-CREATE TABLE `userAddress_dhruva` (
-  `ua_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `ua_userId` int(11) NOT NULL ,
-  `ua_street` varchar(36) NOT NULL,
-  `ua_suite` varchar(20) NOT NULL,
-  `ua_city` varchar(36) NOT NULL,
-  `ua_zipcode` varchar(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE users_dhruva (
+  u_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('users_dhruva_seq'),
+  u_name varchar(36) NOT NULL,
+  u_username varchar(20) NOT NULL UNIQUE,
+  u_email varchar(36) NOT NULL,
+  u_website varchar(36) NOT NULL,
+  u_phone varchar(36) NOT NULL
+) ;
 
-DROP TABLE IF EXISTS `userAddressGeo_dhruva`;
-CREATE TABLE `userAddressGeo_dhruva` (
-  `uag_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `uag_addressId` int(11) NOT NULL ,
-  `uag_lat` varchar(36) NOT NULL,
-  `uag_lng` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS userAddress_dhruva;
+DROP SEQUENCE IF EXISTS userAddress_dhruva_seq;
+CREATE SEQUENCE userAddress_dhruva_seq;
 
-DROP TABLE IF EXISTS `userCompany_dhruva`;
-CREATE TABLE `userCompany_dhruva` (
-  `uc_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `uc_userId` int(11) NOT NULL ,
-  `uc_name` varchar(36) NOT NULL,
-  `uc_catchPhrase` varchar(20) NOT NULL,
-  `uc_bs` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE userAddress_dhruva (
+  ua_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('userAddress_dhruva_seq'),
+  ua_userId int NOT NULL ,
+  ua_street varchar(36) NOT NULL,
+  ua_suite varchar(20) NOT NULL,
+  ua_city varchar(36) NOT NULL,
+  ua_zipcode varchar(36) NOT NULL
+) ;
 
-DROP TABLE IF EXISTS `posts_dhruva`;
-CREATE TABLE `posts_dhruva` (
-  `p_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `p_userId` int(11) NOT NULL ,
-  `p_title` varchar(36) NOT NULL,
-  `p_body` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS userAddressGeo_dhruva;
+DROP SEQUENCE IF EXISTS userAddressGeo_dhruva_seq;
+CREATE SEQUENCE userAddressGeo_dhruva_seq;
 
-DROP TABLE IF EXISTS `comments_dhruva`;
-CREATE TABLE `comments_dhruva` (
-  `c_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `c_postId` int(11) NOT NULL ,
-  `c_name` varchar(36) NOT NULL,
-  `c_email` varchar(36) NOT NULL,
-  `c_body` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE userAddressGeo_dhruva (
+  uag_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('userAddressGeo_dhruva_seq'),
+  uag_addressId int NOT NULL ,
+  uag_lat varchar(36) NOT NULL,
+  uag_lng varchar(20) NOT NULL
+) ;
 
-DROP TABLE IF EXISTS `albums_dhruva`;
-CREATE TABLE `albums_dhruva` (
-  `a_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `a_userId` int(11) NOT NULL ,
-  `a_title` varchar(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS userCompany_dhruva;
+DROP SEQUENCE IF EXISTS userCompany_dhruva_seq;
+CREATE SEQUENCE userCompany_dhruva_seq;
 
-DROP TABLE IF EXISTS `photos_dhruva`;
-CREATE TABLE `photos_dhruva` (
-  `p_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `p_albumId` int(11) NOT NULL ,
-  `p_title` varchar(36) NOT NULL,
-  `p_url` varchar(36) NOT NULL,
-  `p_thumbnailUrl` varchar(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE userCompany_dhruva (
+  uc_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('userCompany_dhruva_seq'),
+  uc_userId int NOT NULL ,
+  uc_name varchar(36) NOT NULL,
+  uc_catchPhrase varchar(100) NOT NULL,
+  uc_bs varchar(100) NOT NULL
+) ;
 
-DROP TABLE IF EXISTS `todos_dhruva`;
-CREATE TABLE `todos_dhruva` (
-  `t_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `t_userId` int(11) NOT NULL ,
-  `t_title` varchar(36) NOT NULL,
-  `t_completed` boolean NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS posts_dhruva;
+DROP SEQUENCE IF EXISTS posts_dhruva_seq;
+CREATE SEQUENCE posts_dhruva_seq;
+
+CREATE TABLE posts_dhruva (
+  p_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('posts_dhruva_seq'),
+  p_userId int NOT NULL ,
+  p_title varchar(100) NOT NULL,
+  p_body text NOT NULL
+) ;
+
+DROP TABLE IF EXISTS comments_dhruva;
+DROP SEQUENCE IF EXISTS comments_dhruva_seq;
+CREATE SEQUENCE comments_dhruva_seq;
+
+CREATE TABLE comments_dhruva (
+  c_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('comments_dhruva_seq'),
+  c_postId int NOT NULL ,
+  c_name varchar(100) NOT NULL,
+  c_email varchar(100) NOT NULL,
+  c_body text NOT NULL
+) ;
+
+DROP TABLE IF EXISTS albums_dhruva;
+DROP SEQUENCE IF EXISTS albums_dhruva_seq;
+CREATE SEQUENCE albums_dhruva_seq;
+
+CREATE TABLE albums_dhruva (
+  a_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('albums_dhruva_seq'),
+  a_userId int NOT NULL ,
+  a_title varchar(100) NOT NULL
+) ;
+
+DROP TABLE IF EXISTS photos_dhruva;
+DROP SEQUENCE IF EXISTS photos_dhruva_seq;
+CREATE SEQUENCE photos_dhruva_seq;
+
+CREATE TABLE photos_dhruva (
+  p_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('photos_dhruva_seq'),
+  p_albumId int NOT NULL ,
+  p_title varchar(100) NOT NULL,
+  p_url varchar(100) NOT NULL,
+  p_thumbnailUrl varchar(100) NOT NULL
+) ;
+
+DROP TABLE IF EXISTS todos_dhruva;
+DROP SEQUENCE IF EXISTS todos_dhruva_seq;
+CREATE SEQUENCE todos_dhruva_seq;
+
+CREATE TABLE todos_dhruva (
+  t_id int NOT NULL PRIMARY KEY DEFAULT NEXTVAL ('todos_dhruva_seq'),
+  t_userId int NOT NULL ,
+  t_title varchar(100) NOT NULL,
+  t_completed boolean NOT NULL
+) ;
